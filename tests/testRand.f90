44d8bba63126cb1283a9c1e780aa039317453fde
d.broemmel <d.broemmel@fz-juelich.de>
2018-03-19 15:48:31 +0100

    Move from deprecated to modern Fortran. Removed OpenMP dep in test. Small change to seed/init of rng.

--- tests/testRand.f90
+++ tests/testRand.f90
@@ -1,29 +1,28 @@
 program testRand
    use frand123
-   use omp_lib
    implicit none
 
    integer, parameter :: numRndNbrs = 1000 * 1000 * 1000
 
    integer( kind = state_kind ), dimension( state_size ) :: state
    real( kind = res_kind ), dimension(:), allocatable :: res
-   integer( kind = state_kind ), dimension( 2 ) :: seed
 
-   double precision :: startTime, stopTime
+   integer :: out_unit
+   real :: startTime, stopTime
 
-   call seed_rand( state, 0, 0, seed )
+   call init_rand( state, 0, 0 )
    
    allocate( res( numRndNbrs ) )
 
-   startTime = omp_get_wtime()
+   call cpu_time(startTime)
    call frand123Dble( state, res )
-   stopTime = omp_get_wtime()
+   call cpu_time(stopTime)
 
-   write(*,'("time elapsed: ", D13.6)') stopTime - startTime
+   write(*,'("time elapsed: ", e13.6)') stopTime - startTime
 
    !write(*,*) res
 
-   open( unit = 124, file = 'rand.out', access = 'stream', status = 'replace' )
-   write( 124 ) res
-   close( 124 )
+   open( newunit = out_unit, file = 'rand.out', access = 'stream', status = 'replace' )
+   write( out_unit ) res
+   close( out_unit )
 end program
