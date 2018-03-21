8ae6c9f31fe74b614aa8040d018db296440f1bb0
Philipp Otte <p.otte@fz-juelich.de>
2018-03-21 10:19:29 +0100

    Added support for generating random signed ints
    Improved in-code documentation of functions

--- wrapper/rand123wrapper.c
+++ wrapper/rand123wrapper.c
@@ -68,7 +68,7 @@
    static const float  summand_float  = 0.5f;
 
    /*
-    * Function ars4x32_u01 calculates for single precision random numbers
+    * Function ars4x32_u01 calculates four single precision random numbers
     * uniformly distributed in [0,1].
     *
     * Arguments: state: four elements holding
@@ -118,6 +118,50 @@
       }
       return;
    }
+   
+   /*
+    * Function ars2x64_int calculates two 64 bit signed integers
+    *
+    * Arguments: state: four elements holding
+    *                   counter: first  128 bit
+    *                   key:     second 128 bit
+    *            res:   adress to storage for 2 64 bit signed integers
+    */
+   void ars2x64_int( int64_t *state, int64_t *res )
+   {
+      // extract counter and key from state
+      ars4x32_ctr_t *ctr_ars = (ars4x32_ctr_t*)&state[0];
+      ars4x32_key_t *key_ars = (ars4x32_key_t*)&state[2];
+      // calc uniformly distributed integers
+      ars4x32_ctr_t resArs = ars4x32_R( 6, *ctr_ars, *key_ars );
+      // store in res and reinterpret as int64_t
+      res[0] = *((int64_t*)&resArs.v[0]);
+      res[1] = *((int64_t*)&resArs.v[2]);
+      return;
+   }
+   
+   /*
+    * Function ars4x32_int calculates four 32 bit signed integers
+    *
+    * Arguments: state: four elements holding
+    *                   counter: first  128 bit
+    *                   key:     second 128 bit
+    *            res:   adress to storage for 4 32 bit signed integers
+    */
+   void ars4x32( int64_t *state, int32_t *res )
+   {
+      // extract counter and key from state
+      ars4x32_ctr_t *ctr_ars = (ars4x32_ctr_t*)&state[0];
+      ars4x32_key_t *key_ars = (ars4x32_key_t*)&state[2];
+      // calc uniformly distributed integers
+      ars4x32_ctr_t resArs = ars4x32_R( 6, *ctr_ars, *key_ars );
+      // store in res and reinterpret as int64_t
+      res[0] = *((int32_t*)&resArs.v[0]);
+      res[1] = *((int32_t*)&resArs.v[1]);
+      res[2] = *((int32_t*)&resArs.v[2]);
+      res[3] = *((int32_t*)&resArs.v[3]);
+      return;
+   }
 #else
    #include <threefry.h>
 
@@ -137,7 +181,7 @@
    static const double summand_double = 0.5 / ( (double)UINT64_MAX + 1. );
 
    /*
-    * Function threefry2x64 returns 2 double precision reals
+    * Function threefry2x64_u01 returns 2 double precision reals
     *
     * Arguments: state: four elements holding
     *                   counter: first  128 bit
@@ -173,7 +217,7 @@
    static const float summand_float  = 0.5 / ( (float)UINT32_MAX + 400.f );
 
    /*
-    * Function threefry4x32 returns 4 single precision reals
+    * Function threefry4x32_u01 returns 4 single precision reals
     *
     * Arguments: state: four elements holding
     *                   counter: first  128 bit
@@ -192,6 +236,66 @@
       res[1] = (float)resInt.v[1] * factor_float + summand_float;
       res[2] = (float)resInt.v[2] * factor_float + summand_float;
       res[3] = (float)resInt.v[3] * factor_float + summand_float;
+      // advance counter
+      if( ctr_threefry->v[0] < UINT64_MAX )
+         ctr_threefry->v[0]++;
+      else
+      {
+         ctr_threefry->v[0] = 0;
+         ctr_threefry->v[1]++;
+      }
+      return;
+   }
+
+   /*
+    * Function threefry2x64_int returns 2 64 bit signed integers
+    *
+    * Arguments: state: four elements holding
+    *                   counter: first  128 bit
+    *                   key:     second 128 bit
+    *            res:   address to storage for 2 64 bit signed integers
+    */
+   void threefry2x64_int( int64_t *state, int64_t *res )
+   {
+      // extract counter and key from state
+      threefry2x64_ctr_t *ctr_threefry = (threefry2x64_ctr_t*)&state[0];
+      threefry2x64_key_t *key_threefry = (threefry2x64_key_t*)&state[2];
+      // calc uniformly distributed integers
+      threefry2x64_ctr_t resThreefry = threefry2x64_R( 13, *ctr_threefry, *key_threefry );
+      // reinterprete as signed 64 bit integer
+      res[0] = *((int64_t*)&resThreefry.v[0]);
+      res[1] = *((int64_t*)&resThreefry.v[2]);
+      // advance counter
+      if( ctr_threefry->v[0] < UINT64_MAX )
+         ctr_threefry->v[0]++;
+      else
+      {
+         ctr_threefry->v[0] = 0;
+         ctr_threefry->v[1]++;
+      }
+      return;
+   }
+
+   /*
+    * Function threefry4x32 returns 4 32 bit signed integers
+    *
+    * Arguments: state: four elements holding
+    *                   counter: first  128 bit
+    *                   key:     second 128 bit
+    *            res:     address to storage for 4 32 bit signed integers
+    */
+   void threefry4x32_int( int64_t *state, int32_t *res )
+   {
+      // extract counter and key from state
+      threefry4x32_ctr_t *ctr_threefry = (threefry4x32_ctr_t*)&state[0];
+      threefry4x32_key_t *key_threefry = (threefry4x32_key_t*)&state[2];
+      // calc uniformly distributed integers
+      threefry4x32_ctr_t resInt = threefry4x32_R( 12, *ctr_threefry, *key_threefry );
+      // reinterprete as 32 bit signed integer
+      res[0] = *((int32_t*)&resInt.v[0]);
+      res[1] = *((int32_t*)&resInt.v[1]);
+      res[2] = *((int32_t*)&resInt.v[2]);
+      res[3] = *((int32_t*)&resInt.v[3]);
       // advance counter
       if( ctr_threefry->v[0] < UINT64_MAX )
          ctr_threefry->v[0]++;
