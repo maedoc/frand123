981343680051b6db99f19be739d9acaa31d0ef31
Philipp Otte <p.otte@fz-juelich.de>
2018-03-23 10:09:36 +0100

    Forgot this file in previous commit

--- tests/testAccuracyFloats.c
+++ tests/testAccuracyFloats.c
@@ -5,16 +5,13 @@
 
 #include <stdio.h>
 #include <stdint.h>
-
-static const float enlarger = 257.f;
+#include "../wrapper/frand123enlarger.h"
 
 int main()
 {
    const float rec = 1.f / ( (float)UINT32_MAX + enlarger );
    float res_max = (float)INT32_MAX * rec;
    float res_min = (float)INT32_MIN * rec;
-   printf( "enlarger=%f\n", enlarger );
-   printf( "res_min=%.20f res_max=%.20f\n", res_min, res_max );
    if( ( res_max < 0.5f ) && ( res_min > -0.5f ) )
    {
       printf( "passed accuracy test for floats\n" );
